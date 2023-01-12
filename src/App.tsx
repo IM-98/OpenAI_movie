976d561a58a9444ba447f073a7f97d2dd207772b
IM-98 <mansourilies@gmail.com>
2023-01-12 20:10:50 +0100

    fix css

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import './App.css';
 import Form from './components/Form/Form';
 
+
+
 function App() {
   return (
-    <div className="container">
-      <Form/>
-    </div>
+   <>
+      <div className="container">
+        <Form/>
+      </div>
+   </>
   );
 }
 
